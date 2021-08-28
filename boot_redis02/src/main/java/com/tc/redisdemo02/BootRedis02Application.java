f284cc523313460675806aa53010032908d931cf
tangchen <tc15542726981@163.com>
2021-08-28 17:45:18 +0800

    redisdemo

---
+++ boot_redis02/src/main/java/com/tc/redisdemo02/BootRedis02Application.java
@@ -0,0 +1,13 @@
+package com.tc.redisdemo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedis02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedis02Application.class, args);
+    }
+
+}
---
+++ boot_redis02/src/main/java/com/tc/redisdemo02/controller/GoodController.java
@@ -0,0 +1,34 @@
+package com.tc.redisdemo02.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodController {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Value("${server.port}")
+    private  String serverPort;
+
+    @GetMapping("/buy_Goods")
+    public String buy_Goods(){
+        String result = stringRedisTemplate.opsForValue().get("goods:001");
+        int goodNumber = result == null ? 0 : Integer.parseInt(result);
+
+        if(goodNumber > 0){
+            int realNumber = goodNumber - 1;
+            stringRedisTemplate.opsForValue().set("goods:001",String.valueOf(realNumber));
+            System.out.println("成功买到商品，库存还剩下："+realNumber+"件"+"\t 服务提供端口 "+serverPort);
+            return "成功买到商品，库存还剩下："+realNumber+"件"+"\t 服务提供端口 "+serverPort;
+        }else {
+            System.out.println("商品已经售完，欢迎下次光临 "+"\t 服务提供端口 "+serverPort);
+            return "商品已经售完，欢迎下次光临 "+"\t 服务提供端口 "+serverPort;
+        }
+    }
+
+}

14cbbbec147188932bcc0c0f40654c0afe86fd4e
tangchen <tc15542726981@163.com>
2021-08-29 17:11:08 +0800

    redis分布式锁

--- boot_redis02/src/main/java/com/tc/redisdemo02/config/RedisConfig.java
+++ boot_redis02/src/main/java/com/tc/redisdemo02/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.tc.redisdemo02.config;
 
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -20,5 +22,14 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
+    }
+
+    @Bean
+    public Redisson redisson(){
+        Config config = new Config();
+
+        config.useSingleServer().setAddress("redis://192.168.152.128:6379").setDatabase(0);
+
+        return (Redisson) Redisson.create(config);
     }
 }
--- boot_redis02/src/main/java/com/tc/redisdemo02/controller/GoodController.java
+++ boot_redis02/src/main/java/com/tc/redisdemo02/controller/GoodController.java
@@ -1,13 +1,20 @@
 package com.tc.redisdemo02.controller;
 
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class GoodController {
+
+    public static final String REDIS_LOCK = "redisLock";
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
@@ -15,19 +22,36 @@ public class GoodController {
     @Value("${server.port}")
     private  String serverPort;
 
-    @GetMapping("/buy_Goods")
-    public String buy_Goods(){
-        String result = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodNumber = result == null ? 0 : Integer.parseInt(result);
+    @Autowired
+    private Redisson redisson;
 
-        if(goodNumber > 0){
-            int realNumber = goodNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001",String.valueOf(realNumber));
-            System.out.println("成功买到商品，库存还剩下："+realNumber+"件"+"\t 服务提供端口 "+serverPort);
-            return "成功买到商品，库存还剩下："+realNumber+"件"+"\t 服务提供端口 "+serverPort;
-        }else {
-            System.out.println("商品已经售完，欢迎下次光临 "+"\t 服务提供端口 "+serverPort);
-            return "商品已经售完，欢迎下次光临 "+"\t 服务提供端口 "+serverPort;
+    @GetMapping("/buy_Goods")
+    public String buy_Goods() throws Exception {
+
+        String value = UUID.randomUUID().toString()+Thread.currentThread().getName();
+
+        RLock redissonLock = redisson.getLock(REDIS_LOCK);
+
+        redissonLock.lock();
+        try {
+            String result = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodNumber = result == null ? 0 : Integer.parseInt(result);
+
+            if(goodNumber > 0){
+                int realNumber = goodNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001",String.valueOf(realNumber));
+                System.out.println("成功买到商品，库存还剩下："+realNumber+"件"+"\t 服务提供端口 "+serverPort);
+                return "成功买到商品，库存还剩下："+realNumber+"件"+"\t 服务提供端口 "+serverPort;
+            }else {
+                System.out.println("商品已经售完，欢迎下次光临 "+"\t 服务提供端口 "+serverPort);
+                return "商品已经售完，欢迎下次光临 "+"\t 服务提供端口 "+serverPort;
+            }
+        } finally {
+            if(redissonLock.isLocked()){
+                if(redissonLock.isHeldByCurrentThread()){
+                    redissonLock.unlock();
+                }
+            }
         }
     }
 
